e373fccc3d966435c77371c5f556e476862c8dcb
YujieKONG <zjukyj@163.com>
2023-08-12 12:42:37 +0800

    init all homework

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -26,6 +26,13 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     // TODO: Implement this function
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
+    float rotation_angle_radian = rotation_angle * MY_PI / 180;
+    float cos = std::cos(rotation_angle);
+    float sin = std::sin(rotation_angle);
+    model(0, 0) = cos;
+    model(0, 1) = -sin;
+    model(1, 0) = sin;
+    model(1, 1) = cos;
 
     return model;
 }
@@ -40,6 +47,37 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
     // TODO: Implement this function
     // Create the projection matrix for the given parameters.
     // Then return it.
+
+    // Compute l, r, b, t
+    float t = std::tan((eye_fov * MY_PI / 180) / 2) * fabs(zNear);
+    float b = -t;
+    float r = aspect_ratio * t;
+    float l = -r;
+
+    // Orthographic projection
+    // 1. Translate to origin
+    Eigen::Matrix4f translate = Eigen::Matrix4f::Identity();
+    translate(0, 3) = - (r + l) / 2;
+    translate(1, 3) = - (t + b) / 2;
+    translate(2, 3) = - (zNear + zFar) / 2;
+    // 2. Sclae to [-1,1]^3
+    Eigen::Matrix4f scale = Eigen::Matrix4f::Identity();
+    scale(0, 0) = 2 / (r - l);
+    scale(1, 1) = 2 / (t - b);
+    scale(2, 2) = 2 / (zNear - zFar);
+    // 3. get Orthographic projection
+    Eigen::Matrix4f ortho = scale * translate;
+
+    // Get Matrix_persp2ortho
+    Eigen::Matrix4f persp2ortho = Eigen::Matrix4f::Zero();
+    persp2ortho(0, 0) = zNear;
+    persp2ortho(1, 1) = zNear;
+    persp2ortho(2, 2) = zNear + zFar;
+    persp2ortho(2, 3) = - zNear * zFar;
+    persp2ortho(3, 2) = 1;
+
+    // Get Perspective projection
+    projection = ortho * persp2ortho;
 
     return projection;
 }
