d97a31a569e38284a17750f16ec38559854c0c6f
YujieKONG <zjukyj@163.com>
2023-08-27 20:47:57 +0800

    hw7 add thread

--- hw7/Renderer.cpp
+++ hw7/Renderer.cpp
@@ -24,8 +24,9 @@ void Renderer::Render(const Scene& scene)
     int m = 0;
 
     // change the spp value to change sample ammount
-    int spp = 16;
+    int spp = 256;
     std::cout << "SPP: " << spp << "\n";
+    #pragma omp parallel for shared(m)
     for (uint32_t j = 0; j < scene.height; ++j) {
         for (uint32_t i = 0; i < scene.width; ++i) {
             // generate primary ray direction
@@ -35,11 +36,11 @@ void Renderer::Render(const Scene& scene)
 
             Vector3f dir = normalize(Vector3f(-x, y, 1));
             for (int k = 0; k < spp; k++){
-                framebuffer[m] += scene.castRay(Ray(eye_pos, dir), 0) / spp;  
+                framebuffer[j * scene.width + i] += scene.castRay(Ray(eye_pos, dir), 0) / spp;
             }
-            m++;
         }
-        UpdateProgress(j / (float)scene.height);
+        UpdateProgress(m / (float)scene.height);
+        m++;
     }
     UpdateProgress(1.f);
 
@@ -53,5 +54,5 @@ void Renderer::Render(const Scene& scene)
         color[2] = (unsigned char)(255 * std::pow(clamp(0, 1, framebuffer[i].z), 0.6f));
         fwrite(color, 1, 3, fp);
     }
-    fclose(fp);    
+    fclose(fp);
 }
